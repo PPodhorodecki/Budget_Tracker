16b9cc2d0eb3b6758f71be34418d6fa2d15886c3
Emek1902 <autowrapper@gmail.com>
2022-01-06 13:03:45 +0100

    Added models

--- budgettracker_project/budgettracker_app/models.py
+++ budgettracker_project/budgettracker_app/models.py
@@ -14,3 +14,35 @@ class User(models.Model):
 
 class Session(models.Model):
     session_name = models.IntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=32)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    pay_month = models.DateField()
+    pay_year = models.DateField()
+    deadline = models.DateField(null=True)
+    continuity = models.BooleanField(default=False)
+    continuity_months = models.IntegerField(default=1)
+    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Note(models.Model):
+    text = models.TextField()
+    mod_date = models.DateField()
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+
+
+class Archive(models.Model):
+    name = models.CharField(max_length=32)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    paid_date = models.DateField()
+    expanse = models.ForeignKey(Expense, on_delete=models.CASCADE)
