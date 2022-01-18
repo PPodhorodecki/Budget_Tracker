3d776dfbdbe3958ee2aed7ef45c742d1482bb817
Emek1902 <autowrapper@gmail.com>
2022-01-18 20:33:48 +0100

    success added new expense, part of validation left

--- budgettracker_project/budgettracker_app/views.py
+++ budgettracker_project/budgettracker_app/views.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta, datetime
 from dateutil.relativedelta import relativedelta
 from django.shortcuts import render, redirect
 from django.views import View
@@ -87,12 +87,13 @@ class Main(View):
                 ctx_main['expense_category'] = expense_category
                 exp_empty = []
                 if expense_name == "":
-                    exp_empty.append(expense_name)
+                    exp_empty.append('expense_name')
                 if expense_value == "":
-                    exp_empty.append(expense_value)
+                    exp_empty.append('expense_value')
                 if len(exp_empty) > 0:
-                    expense_info = "Pole nie może pozostać puste."
-                    ctx_main['expense_info'] = expense_info
+                    exp_empty_field = "Pole nie może pozostać puste."
+                    ctx_main['exp_empty_field'] = exp_empty_field
+                    ctx_main['exp_empty'] = exp_empty
                     return render(request, 'main.html', context=ctx_main)
                 # if isinstance(expense_value, (int, float)) is not True:
                 #     expense_info = "Kwota płatności musi być liczbą całkowitą lub dziesiętną."
@@ -101,7 +102,8 @@ class Main(View):
                 expense_deadline = request.POST.get('exp_deadline')
                 ctx_main['expense_deadline'] = expense_deadline
                 expense_continuity = request.POST.get('continuity')
-                if expense_continuity == True and expense_deadline != "":
+                if expense_continuity == 'true' and expense_deadline != "":
+                    exp_continuity = True
                     expense_days = request.POST.get('days_amount')
                     expense_weeks = request.POST.get('weeks_amount')
                     expense_months = request.POST.get('months_amount')
@@ -110,14 +112,14 @@ class Main(View):
                     ctx_main['expense_weeks'] = expense_weeks
                     ctx_main['expense_months'] = expense_months
                     ctx_main['expense_amount'] = expense_amount
-                    if expense_days != "" and isinstance(expense_days, int) is True and expense_weeks == "" and expense_months == "":
-                        next_expense = expense_deadline + relativedelta(days=+expense_days)
+                    if expense_days != "" and expense_weeks == "" and expense_months == "":
+                        next_expense = datetime.strptime(expense_deadline, '%Y-%m-%d')+timedelta(days=int(expense_days))
                         period = f'{expense_days}_days'
-                    elif expense_weeks != "" and isinstance(expense_weeks, int) is True and expense_days == "" and expense_months == "":
-                        next_expense = expense_deadline + relativedelta(weeks=+expense_weeks)
+                    elif expense_weeks != "" and expense_days == "" and expense_months == "":
+                        next_expense = datetime.strptime(expense_deadline, '%Y-%m-%d')+timedelta(weeks=int(expense_weeks))
                         period = f'{expense_weeks}_weeks'
-                    elif expense_months != "" and isinstance(expense_months, int) is True and expense_days == "" and expense_weeks == "":
-                        next_expense = expense_deadline + relativedelta(months=+expense_months)
+                    elif expense_months != "" and expense_days == "" and expense_weeks == "":
+                        next_expense = datetime.strptime(expense_deadline, '%Y-%m-%d')+relativedelta(months=int(expense_months))
                         period = f'{expense_months}_months'
                     elif expense_months == "" and expense_days == "" and expense_weeks == "":
                         expense_info = "Musisz wybrać okres częstotliwości występowania płatności."
@@ -131,27 +133,31 @@ class Main(View):
                         exp_amount = 1000
                     else:
                         exp_amount = request.POST.get('continuity_number')
-                        if exp_amount == "" or isinstance(exp_amount, int) is not True:
+                        if exp_amount == "":
                             expense_info = "Wartość ilości cykli płatności jest nieprawidlowa lub pusta."
                             ctx_main['expense_info'] = expense_info
                             return render(request, 'main.html', context=ctx_main)
-                elif expense_continuity == True and expense_deadline == "":
+                elif expense_continuity == 'true' and expense_deadline == "":
                     expense_info = "Przy płatnościach cyklicznych należy podać termin płatności"
                     ctx_main['expense_info'] = expense_info
                     return render(request, 'main.html', context=ctx_main)
                 expense_ispaid = request.POST.get('is_paid')
+                if expense_ispaid == 'true':
+                    ispaid = True
+                else:
+                    ispaid = False
                 expense = Expense()
                 expense.name = expense_name
                 expense.value = expense_value
                 expense.create = date.today()
                 #expense.deadline = expense_deadline
-                if expense_continuity == True:
+                if exp_continuity == True:
                     expense.deadline = expense_deadline
-                    expense.continuity = expense_continuity
+                    expense.continuity = exp_continuity
                     expense.exp_amount = exp_amount
                     expense.period_delta = period
                     expense.next_exp = next_expense
-                expense.is_paid = expense_ispaid
+                expense.is_paid = ispaid
                 expense.user = request.user
                 expense.category = Category.objects.get(name=expense_category)
                 expense.save()
